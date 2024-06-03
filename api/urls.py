244f55883946c8eae0a87b24dbc65e15186e5ade
Giwa-ThankGod <giwathankgod101@gmail.com>
2024-06-03 09:05:56 +0100

    refactor(urls.py): Allows '/' flexibility for API POST endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path
+from django.urls import re_path
 from api import views
 
 urlpatterns = [
     # Class Attendance Enpoints
-    path('classes/', views.ClassAttendanceView.as_view()),
-    path('classes/attendance/by-class/', views.FetchClassAttendanceByClassNameView.as_view()),
-    path('classes/attendance/by-workspace/', views.FetchClassAttendanceByWorkspaceView.as_view()),
+    re_path(r'^classes$', views.ClassAttendanceView.as_view()),
+    re_path(r'^classes/attendance/by-class$', views.FetchClassAttendanceByClassNameView.as_view()),
+    re_path(r'^classes/attendance/by-workspace$', views.FetchClassAttendanceByWorkspaceView.as_view()),
 
     # Bus Attendance Endpoints
-    path('buses/', views.BusAttendanceView.as_view()),
-    path('buses/attendance/by-bus/', views.FetchBusAttendanceByBusNameView.as_view()),
-    path('buses/attendance/by-workspace/', views.FetchBusAttendanceByWorkspaceView.as_view()),
+    re_path(r'^buses$', views.BusAttendanceView.as_view()),
+    re_path(r'^buses/attendance/by-bus$', views.FetchBusAttendanceByBusNameView.as_view()),
+    re_path(r'^buses/attendance/by-workspace$', views.FetchBusAttendanceByWorkspaceView.as_view()),
 ]
